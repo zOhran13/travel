5814b8ac5a1b5dbdee6f3059a577a2db1fa44524
zlata <zohran1@etf.unsa.ba>
2023-02-10 11:51:34 +0100

    added method in userManager for login

--- src/main/java/ba/unsa/etf/rpr/business/UserManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/UserManager.java
@@ -1,4 +1,21 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.ArrangementException;
+
 public class UserManager {
+    public User login(String email, String password) throws ArrangementException {
+
+        if(email == null || password.equals("")){
+            throw new ArrangementException("All fields have to be filled");
+        }
+        User u = DaoFactory.userDao().getByEmail(email);
+
+        if(!u.getPassword().equals(password)){
+            throw new ArrangementException("Password incorrect");
+        }
+        return u;
+
+    }
 }
