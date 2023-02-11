097445df41203719b5ad9633f8702a52ed3fdf10
zlata <zohran1@etf.unsa.ba>
2023-02-11 23:27:50 +0100

    Add style for new window

--- src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
@@ -1,4 +1,21 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class RegistrationController {
+    public Label nameId;
+    public Label surnameId;
+    public Label addressId;
+    public Label phoneId;
+    public Label mailId;
+    public Button btnCancelId;
+    public Button btnOkId;
+
+    public void backToLogin(ActionEvent actionEvent) {
+    }
+
+    public void resgistrationForUser(ActionEvent actionEvent) {
+    }
 }
