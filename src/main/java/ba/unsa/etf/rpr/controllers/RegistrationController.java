e7f9a07b0cd54db5a1abec96f6d8d7b580104913
zlata <zohran1@etf.unsa.ba>
2023-02-12 00:21:52 +0100

    Method for registration

--- src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
@@ -1,11 +1,13 @@
 package ba.unsa.etf.rpr.controllers;
 
 
+import ba.unsa.etf.rpr.business.UserManager;
 import ba.unsa.etf.rpr.domain.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -44,14 +46,35 @@ public class RegistrationController {
     }
 
     public void registrationForUser(ActionEvent actionEvent) {
-        User u = new User();
-        u.setName(nameId.getText());
-        u.setSurname(surnameId.getText());
-        u.setAddress(addressId.getText());
-        u.setPhoneNumber(phoneId.getText());
-        u.setPassword(passwordId.getText());
-        u.setEmail(emailId.getText());
+        try {
+            User u = new User();
+            u.setName(nameId.getText());
+            u.setSurname(surnameId.getText());
+            u.setAddress(addressId.getText());
+            u.setPhoneNumber(phoneId.getText());
+            u.setPassword(passwordId.getText());
+            u.setEmail(emailId.getText());
+            UserManager.register(u);
 
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Registration");
+            alert.setHeaderText("Results:");
+            alert.setContentText("Thank you for registration! Please sign in");
+            alert.showAndWait();
 
+            Stage stage = (Stage) btnOkId.getScene().getWindow();
+            stage.close();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+            fxmlLoader.setController(new LoginController());
+            Parent root = fxmlLoader.load();
+            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.setResizable(false);
+            stage.show();
+
+        } catch (Exception e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
     }
+
+
 }
