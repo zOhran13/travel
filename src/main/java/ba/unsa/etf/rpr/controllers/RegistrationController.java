8b10f43af0b119d03c07bd543e0603aec36f189f
zlata <zohran1@etf.unsa.ba>
2023-02-11 23:35:51 +0100

    Cancel registration

--- src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
@@ -1,8 +1,15 @@
 package ba.unsa.etf.rpr.controllers;
 
+
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class RegistrationController {
     public Label nameId;
@@ -14,8 +21,22 @@ public class RegistrationController {
     public Button btnOkId;
 
     public void backToLogin(ActionEvent actionEvent) {
+        try{
+            Stage stage = (Stage) btnCancelId.getScene().getWindow();
+            stage.close();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+            fxmlLoader.setController(new LoginController());
+            Parent root = fxmlLoader.load();
+            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.setResizable(false);
+            stage.show();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 
-    public void resgistrationForUser(ActionEvent actionEvent) {
+    public void registrationForUser(ActionEvent actionEvent) {
     }
 }
