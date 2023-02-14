812c7e78c7376640d92f25df1d9310277dfb874c
zlata <zohran1@etf.unsa.ba>
2023-02-14 11:38:54 +0100

    Terminal UI

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -2,10 +2,18 @@ package ba.unsa.etf.rpr;
 
 import ba.unsa.etf.rpr.exceptions.ArrangementException;
 
+import java.util.Scanner;
+
 public class App
 {
     public static void main( String[] args ) throws ArrangementException {
         System.out.println("Hello user. Welcome to travel agency. This is a simple app for booking the best travel arrangements.");
+        System.out.println("If you don't have a account type 1, if you already have one type 2.");
+        Scanner scanner = new Scanner(System.in);
+        if(scanner.nextInt() == 1) {
+
+        }
+
 
     }
 }
