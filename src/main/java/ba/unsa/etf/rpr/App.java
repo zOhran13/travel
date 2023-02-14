6f78db070c3b3dde5c0e343d34bc0b3d8efabf54
zlata <zohran1@etf.unsa.ba>
2023-02-14 11:48:31 +0100

    Login in app

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.ArrangementException;
 
 import java.util.Scanner;
@@ -10,7 +12,21 @@ public class App
         System.out.println("Hello user. Welcome to travel agency. This is a simple app for booking the best travel arrangements.");
         System.out.println("If you don't have a account type 1, if you already have one type 2.");
         Scanner scanner = new Scanner(System.in);
-        if(scanner.nextInt() == 1) {
+        if(scanner.nextInt() == 2) {
+            String email;
+            String password;
+            System.out.println("Email: ");
+            Scanner scanner1 = new Scanner(System.in);
+            email = scanner1.next();
+            System.out.println("Password: ");
+            password = scanner1.next();
+            User user = DaoFactory.userDao().getByEmail(email);
+            if (user != null && user.getPassword().equals(password)){
+                System.out.println("Login successful\n");
+                //break;
+            }
+
+
 
         }
 
