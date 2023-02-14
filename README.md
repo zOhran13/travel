0722c012cbda8496e1693280ebd01e7c0e992ce8
zlata <zohran1@etf.unsa.ba>
2023-02-14 11:25:43 +0100

    Editing readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Travel agency
+## Travel agency
+
+This is a simple app for booking the best travel arrangements.
+
+The user can log in, view the profile and book an arrangement, register.   
--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,3 +1,11 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.exceptions.ArrangementException;
 
+public class App
+{
+    public static void main( String[] args ) throws ArrangementException {
+        System.out.println("Hello user. Welcome to travel agency. This is a simple app for booking the best travel arrangements.");
+
+    }
+}
