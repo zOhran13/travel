02dbe0a8b0e7fc3a0f65dd281c6963f33ca2e1b3
zlata <zohran1@etf.unsa.ba>
2023-02-14 12:45:50 +0100

    Choosing options for user in terminal.

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -9,6 +9,8 @@ import java.util.Scanner;
 public class App
 {
     public static void main( String[] args ) throws ArrangementException {
+
+
         System.out.println("Hello user. Welcome to travel agency. This is a simple app for booking the best travel arrangements.");
         System.out.println("If you don't have a account type 1, if you already have one type 2.");
         Scanner scanner = new Scanner(System.in);
@@ -38,6 +40,33 @@ public class App
                  user = DaoFactory.userDao().getByEmail(email);
 
             }
+            whatDoYouWant(user.getId());
         }
+    }
+    private static void whatDoYouWant(int id){
+        System.out.println("Choose one: ");
+        System.out.println("1: Show me a list of my reservations");
+        System.out.println("2. Make me a reservation");
+        System.out.println("3. Delete a reservation");
+        System.out.println("4. Exit");
+        int action;
+        Scanner scanner = new Scanner(System.in);
+        action = scanner.nextInt();
+        switch (action){
+            case 1 : listOfReservtionOfaUser(id);
+            case 2 :  makeAReservation(id);
+            case 3 : deleteReservation(id);
+            case 4 : System.exit(0);
+        }
+
+    }
+    private static void listOfReservtionOfaUser(int id) {
+
+    }
+    private static void makeAReservation(int id) {
+
+    }
+    private static void deleteReservation(int id) {
+
     }
 }
