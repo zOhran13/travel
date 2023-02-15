88179994271a4203dd5ebb37be77f5cfbbd1fb84
zlata <zohran1@etf.unsa.ba>
2023-02-15 12:02:01 +0100

    javadoc for interface

--- src/main/java/ba/unsa/etf/rpr/dao/ArrangementDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ArrangementDao.java
@@ -2,6 +2,9 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Arrangement;
 
+/**
+ * Interface that extends Dao<T> plus has methods that are needed only for ArrangementDao
+ */
 public interface ArrangementDao extends Dao<Arrangement>{
 
 }
