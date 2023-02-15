3380ec8c3c6b0edb08a647ebad38379a3d3044f9
zlata <zohran1@etf.unsa.ba>
2023-02-15 12:08:48 +0100

    Dao interface javadoc

--- src/main/java/ba/unsa/etf/rpr/dao/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -4,8 +4,18 @@ import ba.unsa.etf.rpr.exceptions.ArrangementException;
 
 import java.util.List;
 
+/**
+ * Dao interface extend by ArrangementDao, ReservationDao, UsersDao
+ * @param <T>
+ */
 public interface Dao <T>{
-T getById(int id)throws ArrangementException;
+    /**
+     * Get entity from database base on ID
+     * @param id
+     * @return
+     * @throws ArrangementException
+     */
+    T getById(int id)throws ArrangementException;
 T add( T item) throws ArrangementException;
 T update(T item) throws ArrangementException;
 void delete(int id) throws ArrangementException;
