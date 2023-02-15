35b7564d9651345bc4bc9eeb754a30d5cb6098c9
zlata <zohran1@etf.unsa.ba>
2023-02-15 12:02:49 +0100

    javadoc for interface

--- src/main/java/ba/unsa/etf/rpr/dao/ReservationDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ReservationDao.java
@@ -5,6 +5,9 @@ import ba.unsa.etf.rpr.exceptions.ArrangementException;
 
 import java.util.List;
 
+/**
+ * Interface that extends Dao<T> plus has methods that are needed only for ReservationDao
+ */
 public interface ReservationDao extends Dao<Reservation> {
     public List<Reservation> reservationsForUser(int userId) throws ArrangementException;
 }
