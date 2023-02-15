9d5067bbccdfcb7208b82c0f8b23a4b621304cdb
zlata <zohran1@etf.unsa.ba>
2023-02-15 12:03:30 +0100

    javadoc for interface

--- src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
@@ -3,6 +3,9 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.ArrangementException;
 
+/**
+ * Interface that extends Dao<T> plus has methods that are needed only for UserDao
+ */
 public interface UserDao extends Dao<User>{
     User getByEmail(String email) throws ArrangementException;
 }
